274c383c497520e6af39e3393075078260dd56be
Ramil <zarram89@gmail.com>
2022-11-22 18:08:38 +0500

    Добавляет react компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
+const Setting = {
+  ERRORS_COUNT: 3,
+};
 
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      errorsCount = {Setting.ERRORS_COUNT}
+    />
   </React.StrictMode>,
-);
+  document.getElementById('root'));
